fb84c3bdde764d57ed8692e2796e51f00b5fdf41
Simon C A <simoncanish@gmail.com>
2026-03-27 11:44:19 +0530

    Added multi-threading on the server side

--- server/server.py
+++ server/server.py
@@ -1,13 +1,18 @@
+import threading
 from socket import *
 port=5000
 serverSocket=socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',port))
 serverSocket.listen(3)
 print("Server ready")
-while True:
-    connectionSocket,addr=serverSocket.accept()
-    message=connectionSocket.recv(1024).decode()
+def handle_client(connectionSocket,addr):
+    message=connectionSocket.recv(1024)
+    message=message.decode()
     message=message+"-processed"
     connectionSocket.send(message.encode())
     connectionSocket.close()
+while True:
+    connectionSocket,addr=serverSocket.accept()
+    thread=threading.Thread(target=handle_client,args=(connectionSocket,addr))
+    thread.start()
     
