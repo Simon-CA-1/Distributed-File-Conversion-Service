4c8174cca91e345f37d9078b1d38fedc3d004b8a
Simon C A <simoncanish@gmail.com>
2026-03-31 01:51:03 +0530

    integrated the worker manager module

--- server/client_handler.py
+++ server/client_handler.py
@@ -1,20 +1,23 @@
-worker=[]
+import worker_manager
 def handle_client(connectionSocket,addr):
     message=connectionSocket.recv(1024)
     message=message.decode()
     if message=="WORKER":
-        worker.append(connectionSocket)
+        worker_manager.add_worker(connectionSocket)
         connectionSocket.send("CONNECTED".encode())
         return
     else:
-        if worker:
-            w=worker[0]
+        if worker_manager.free_workers:
+            w=worker_manager.choose_worker()
+            if(w==None):
+                connectionSocket.close()
+                return
             w.send(message.encode())
             message=w.recv(1024)
             if not message:
                 connectionSocket.close()
                 return
-
+            worker_manager.work_done(w)
             message=message.decode()
             connectionSocket.send(message.encode())
             connectionSocket.close()
