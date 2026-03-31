ca715f8aa26b81d3d5541ec1f74c35d39211805c
Simon C A <simoncanish@gmail.com>
2026-03-31 12:50:36 +0530

    Integrated worker manager for assigning client requests to available workers

--- server/client_handler.py
+++ server/client_handler.py
@@ -1,26 +1,27 @@
 import worker_manager
 def handle_client(connectionSocket,addr):
     message=connectionSocket.recv(1024)
+    if not message:
+        connectionSocket.close()
+        return
     message=message.decode()
     if message=="WORKER":
         worker_manager.add_worker(connectionSocket)
         connectionSocket.send("CONNECTED".encode())
         return
-    else:
-        if worker_manager.free_workers:
-            w=worker_manager.choose_worker()
-            if(w==None):
-                connectionSocket.close()
-                return
-            w.send(message.encode())
-            message=w.recv(1024)
-            if not message:
-                connectionSocket.close()
-                return
-            worker_manager.work_done(w)
-            message=message.decode()
-            connectionSocket.send(message.encode())
-            connectionSocket.close()
-        else:
-            connectionSocket.send("No worker".encode())
-            connectionSocket.close()
+    w=worker_manager.choose_worker()
+    if(w==None):
+        connectionSocket.send("NO WORKER".encode())
+        connectionSocket.close()
+        return
+    w.send(message.encode())
+    message=w.recv(1024)
+    if not message:
+        connectionSocket.send("WORKER DISCONNECTED".encode())
+        connectionSocket.close()
+        return
+    worker_manager.work_done(w)
+    message=message.decode()
+    connectionSocket.send(message.encode())
+    connectionSocket.close()
+        
