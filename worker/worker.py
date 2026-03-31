6070b3ef6ba8fe88ac98f3b021ef0a6f8bbade8a
Simon C A <simoncanish@gmail.com>
2026-04-01 02:10:12 +0530

    Fix file naming and ensure correct output format display

--- worker/worker.py
+++ worker/worker.py
@@ -2,17 +2,16 @@ from converter import convert_file
 from socket import *
 from config import SERVER_HOST,SERVER_PORT
 from file_transfer import receive_file,send_file
+
 workerSocket=socket(AF_INET,SOCK_STREAM)
 workerSocket.connect((SERVER_HOST,SERVER_PORT))
+
 print("WORKER SIDE")
+
 workerSocket.send("WORKER".encode())
+workerSocket.recv(1024)
+
 while True:
-    text=workerSocket.recv(1024)
-    text=text.decode()
-    if not text:
-        break
-    if(text=="CONNECTED"):
-        continue
-    receive_file(workerSocket,"input_file")
-    output_file=convert_file("input_file")
+    receive_file(workerSocket,"input_file.jpg")
+    output_file=convert_file("input_file.jpg")
     send_file(workerSocket,output_file)
