a9b103262173f9c4c970ac59c61da85f0124413f
Simon C A <simoncanish@gmail.com>
2026-04-01 08:30:57 +0530

    added thread safety using locks for scheduler and worker manager

--- server/scheduler.py
+++ server/scheduler.py
@@ -1,9 +1,13 @@
+import threading
+lock=threading.Lock()
 jobs = []
 def add_job(job_size, connectionSocket, message):
-    jobs.append([job_size, connectionSocket, message])
-    jobs.sort(key=lambda x: x[0])
+    with lock:
+        jobs.append([job_size, connectionSocket, message])
+        jobs.sort(key=lambda x: x[0])
 
 def get_next_job():
-    if not jobs:
-        return None
-    return jobs.pop(0)
+    with lock:
+        if not jobs:
+            return None
+        return jobs.pop(0)
--- server/worker_manager.py
+++ server/worker_manager.py
@@ -1,23 +1,29 @@
+import threading
+lock=threading.Lock()
 free_workers=[]
 busy_workers=[]
 
 def add_worker(workerSocket):
-    free_workers.append(workerSocket)
-
-def choose_worker():
-    if not free_workers:
-        return None
-    worker=free_workers.pop(0)
-    busy_workers.append(worker)
-    return worker
-
-def work_done(workerSocket):
-    if workerSocket in busy_workers:
-        busy_workers.remove(workerSocket)
+    with lock:
         free_workers.append(workerSocket)
 
+def choose_worker():
+    with lock:
+        if not free_workers:
+            return None
+        worker=free_workers.pop(0)
+        busy_workers.append(worker)
+        return worker
+
+def work_done(workerSocket):
+    with lock:
+        if workerSocket in busy_workers:
+            busy_workers.remove(workerSocket)
+            free_workers.append(workerSocket)
+
 def remove_worker(workerSocket):
-    if workerSocket in free_workers:
-        free_workers.remove(workerSocket)
-    if workerSocket in busy_workers:
-        busy_workers.remove(workerSocket)
+    with lock:
+        if workerSocket in free_workers:
+            free_workers.remove(workerSocket)
+        if workerSocket in busy_workers:
+            busy_workers.remove(workerSocket)
